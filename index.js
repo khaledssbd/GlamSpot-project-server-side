d25d4a2b7133d7fcd40c2de643cadebc42f7a567
khaledshuvo <khaledsbd@hotmail.com>
2024-05-12 01:03:58 +0600

    delete with count decrease done

--- index.js
+++ index.js
@@ -9,7 +9,21 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+// all middleware
+const corsConfig = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://glamspot-khaled.web.app',
+    'https://ph-a11-client-by-khaled.vercel.app',
+    'https://ph-a11-client-by-khaled.surge.sh',
+    'https://ph-a11-client-by-khaled.netlify.app',
+  ],
+  credentials: true,
+};
+app.use(cors(corsConfig));
+app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2brfitt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,18 +35,240 @@ const client = new MongoClient(uri, {
   },
 });
 
+// my made middlewares
 
+// const logger = async (req, res, next) => {
+//   console.log('called', req.method, req.host, req.url, req.originalUrl);
+//   next();
+// };
+
+const verifyToken = async (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+const cookieOptions = {
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+  secure: process.env.NODE_ENV === 'production',
+};
 
 async function run() {
-
   try {
     const serviceCollection = client.db('glamSpotDB').collection('services');
     const bookingCollection = client.db('glamSpotDB').collection('bookings');
 
-   
+    // auth related API
+    // require('crypto').randomBytes(64).toString('hex')
+    app.post('/getJwtToken', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '6h',
+      });
+      res.cookie('token', token, cookieOptions).send({ success: true });
+    });
+
+    app.post('/deleteJwtToken', async (req, res) => {
+      res
+        .clearCookie('token', { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
+
+    // services related API
+    // Get all services
+    app.get('/all-services', async (req, res) => {
+      const result = await serviceCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Get service details
+    app.get('/service-details/:id', async (req, res) => {
+      const Id = req.params.id;
+      const query = { _id: new ObjectId(Id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Get services by pagination
+    app.get('/all-services-by-pagination', async (req, res) => {
+      const size = parseInt(req.query.size);
+      const page = parseInt(req.query.page) - 1;
+      const sort = req.query.sort;
+      let options = {};
+      if (sort) options = { sort: { serviceName: sort === 'asc' ? 1 : -1 } };
+      const result = await serviceCollection
+        .find({}, options)
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+
+      res.send(result);
+    });
+
+    // Get services count
+    app.get('/services-count', async (req, res) => {
+      const count = await serviceCollection.countDocuments();
+      res.send({ count });
+    });
+
+    // Get search results
+    app.get('/search-services', async (req, res) => {
+      const search = req.query.search;
+      const query = {
+        serviceName: { $regex: search, $options: 'i' },
+      };
+      const result = await serviceCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+    // post a booking
+    app.post('/book-now', verifyToken, async (req, res) => {
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      const newBooking = req.body;
+      const result = await bookingCollection.insertOne(newBooking);
+
+      // increase booking count 1 in service collection
+      const updateDoc = { $inc: { totalBookings: 1 } };
+      const serviceQuery = { _id: new ObjectId(newBooking.serviceId) };
+      const updateBidCount = await serviceCollection.updateOne(
+        serviceQuery,
+        updateDoc
+      );
+
+      res.send(result);
+    });
+
+    // get my bookings
+    app.get('/bookings', verifyToken, async (req, res) => {
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      const query = {
+        customerEmail: req.query.email,
+      };
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // Get service details
+    app.get('/booking-details/:id', async (req, res) => {
+      const BookingID = req.params.id;
+      const query = { _id: new ObjectId(BookingID) };
+      const result = await bookingCollection.findOne(query);
+      res.send(result);
+    });
+
+    // update my booking
+    app.patch('/update-booking/:id', async (req, res) => {
+      const BookingID = req.params.id;
+      console.log(BookingID);
+      const filter = { _id: new ObjectId(BookingID) };
+      // const jobData = req.body;
+      // const updateData = { $set: {...jobData} };
+      const updateData = { $set: req.body };
+      const result = await bookingCollection.updateOne(filter, updateData);
+      res.send(result);
+    });
+
+    // delete my booking
+    app.delete('/delete-booking/:id', verifyToken, async (req, res) => {
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      const query = {
+        _id: new ObjectId(req.params.id),
+      };
+      // decrease booking count 1 in service collection
+      const paiyaGesi = await bookingCollection.findOne(query);
+      const updateDoc = { $inc: { totalBookings: -1 } };
+      const serviceQuery = { _id: new ObjectId(paiyaGesi.serviceId) };
+      const updateBidCount = await serviceCollection.updateOne(
+        serviceQuery,
+        updateDoc
+      );
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //------------------------------------------------------------------------------------------
+
+    app.get('/services', async (req, res) => {
+      const result = await serviceCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/services/:id', async (req, res) => {
+      const Id = req.params.id;
+      const query = { _id: new ObjectId(Id) };
+
+      const options = {
+        // Sort returned documents in ascending order by title (A->Z)
+        // sort: { title: 1 },
+
+        // Sort returned documents in ascending order by title (Z->A)
+        // sort: { title: -1 },
+
+        // (id na caile _id:0 dite hoy coz by default eta diye day...  onno ja ja cai tar por 1 dite hobe)
+        // projection: { _id: 0, title: 1, imdb: 1 },
+        projection: { img: 1, title: 1, price: 1 },
+      };
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    // bookings related API
+    app.get('/bookings', verifyToken, async (req, res) => {
+      // console.log(req.query.email);
+      // console.log('tok tok token', req?.cookies?.token);
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+        // query.email = req.query.email
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post('/bookings', async (req, res) => {
+      const newBooking = req.body;
+      const result = await bookingCollection.insertOne(newBooking);
+      res.send(result);
+    });
+
+    app.patch('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateData = { $set: { status: req.body.status } };
+      const result = await bookingCollection.updateOne(filter, updateData);
+      res.send(result);
+    });
+
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection to DB
-    // await client.db('admin').command({ ping: 1 });
+    await client.db('admin').command({ ping: 1 });
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     );
@@ -44,9 +280,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Car Doctor server is running');
+  res.send('Hellow! From GlamSpot server owner Khaled');
 });
 
 app.listen(port, () => {
-  console.log(`Car Doctor server is running on port: ${port}`);
+  console.log(`GlamSpot server is running on port: ${port}`);
 });
